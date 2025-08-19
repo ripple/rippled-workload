f849b02e37bf55099207d5e055d09f1fbd0e2eee
Michael Legleux <mlegleux@ripple.com>
2025-08-19 08:22:39 -0700

    fix-start-experiment

--- prep_network.sh
+++ prep_network.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 set -x
 
-TEST_NETWORK_DIR="test_network"
-mv ../$TEST_NETWORK_DIR .
-
-## Can't do this here because we need to move it before copying between jobs in github workflow
-#mv ../accounts.json $TEST_NETWORK_DIR/
-
-# update the generated compose file
-sed -i '1i \
-include:\
-  - sidecar-compose.yml\
-  - workload-compose.yml\
-' ${TEST_NETWORK_DIR}/docker-compose.yml
+test_network_dir="${TEST_NETWORK_DIR:-testnet}"
+mv "../${test_network_dir}" .
 
 sed -i '/^\s*hostname:\s*rippled\s*$/a\
     healthcheck:\
       test: ["CMD", "/usr/bin/curl", "--insecure", "https://localhost:51235/health"]\
       interval: 10s\
-      start_period: 45s' ${TEST_NETWORK_DIR}/docker-compose.yml
+      start_period: 45s' ${test_network_dir}/docker-compose.yml
 
 
 # install the workload and sidecar compose files
 for i in sidecar workload; do
-    echo "copying ${i}/docker-compose.yml to ${TEST_NETWORK_DIR}/${i}-compose.yml"
-    cp "${i}/docker-compose.yml" "${TEST_NETWORK_DIR}/${i}-compose.yml"
+    echo "copying ${i}/docker-compose.yml to ${test_network_dir}/${i}-compose.yml"
+    cp "${i}/docker-compose.yml" "${test_network_dir}/${i}-compose.yml"
 done
 
-sed -i "s|^\s*image:\s*rippled:latest|    image: ${RIPPLED_IMAGE}|" ${TEST_NETWORK_DIR}/docker-compose.yml
-sed -Ei '1,/^[[:space:]]*entrypoint:[[:space:]]*\["rippled"\]/! s|^[[:space:]]*entrypoint:[[:space:]]*\["rippled"\]|    entrypoint: ["sh", "-c", "sleep 15 \&\& rippled --net"]|' ${TEST_NETWORK_DIR}/docker-compose.yml
-sed -i "s|^\s*image:\s*\$SIDECAR_IMAGE|    image: ${SIDECAR_IMAGE}|" ${TEST_NETWORK_DIR}/sidecar-compose.yml
-sed -i "s|^\s*image:\s*\$WORKLOAD_IMAGE|    image: ${WORKLOAD_IMAGE}|" ${TEST_NETWORK_DIR}/workload-compose.yml
+sed -i "s|^\s*image:\s*rippled:latest|    image: ${RIPPLED_IMAGE}|" ${test_network_dir}/docker-compose.yml
+sed -Ei '1,/^[[:space:]]*entrypoint:[[:space:]]*\["rippled"\]/! s|^[[:space:]]*entrypoint:[[:space:]]*\["rippled"\]|    entrypoint: ["sh", "-c", "sleep 15 \&\& rippled --net"]|' ${test_network_dir}/docker-compose.yml
+sed -i "s|^\s*image:\s*\$SIDECAR_IMAGE|    image: ${SIDECAR_IMAGE}|" ${test_network_dir}/sidecar-compose.yml
+sed -i "s|^\s*image:\s*\$WORKLOAD_IMAGE|    image: ${WORKLOAD_IMAGE}|" ${test_network_dir}/workload-compose.yml
 
-ls -l "${TEST_NETWORK_DIR}"
-cat "${TEST_NETWORK_DIR}/docker-compose.yml"
-cat "${TEST_NETWORK_DIR}/sidecar-compose.yml"
-cat "${TEST_NETWORK_DIR}/workload-compose.yml"
+ls -l "${test_network_dir}"
+cat "${test_network_dir}/docker-compose.yml"
+cat "${test_network_dir}/sidecar-compose.yml"
+cat "${test_network_dir}/workload-compose.yml"
